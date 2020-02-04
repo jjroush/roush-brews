4d4f0d23b988f810444d4f756daa28c006143b37
Jacob Roush <jacobroushmail@gmail.com>
2020-02-04 16:23:37 -0600

    adds recipe and articlw content types

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,5 +1,21 @@
+import { frontMatter as articles } from './*.mdx'
+import Link from 'next/link';
+
 function ArticlesPage() {
-    return <div>Welcome to Next.js articles!</div>
+
+    const formatPath = (p) => p.replace(/\.mdx$/, '');
+
+    return (
+        <ul>
+            {articles.map(article => (
+                <li key={article.__resourcePath}>
+                    <Link href={formatPath(article.__resourcePath)}>
+                        <a>{article.title}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default ArticlesPage
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,5 +1,21 @@
+import { frontMatter as recipes } from './*.mdx'
+import Link from 'next/link';
+
 function RecipePage() {
-    return <div>Welcome to Next.js recipes!</div>
+
+    const formatPath = (p) => p.replace(/\.mdx$/, '');
+
+    return (
+        <ul>
+            {recipes.map(recipe => (
+                <li key={recipe.__resourcePath}>
+                    <Link href={formatPath(recipe.__resourcePath)}>
+                        <a>{recipe.title}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default RecipePage
