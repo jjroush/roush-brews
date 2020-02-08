edb73088f2eb3a828c3f6902f02b344d07a0db4a
Jacob Roush <jacobroushmail@gmail.com>
2020-02-08 00:30:28 -0600

    added custom now setup and service worker

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
-const withMdxEnhanced = require('next-mdx-enhanced')
-const withOffline = require('next-offline')
+const withMdxEnhanced = require("next-mdx-enhanced");
+const withOffline = require("next-offline");
 
 const nextConfig = {
-    layoutPath: 'mdx-layouts',
-}
+  layoutPath: "mdx-layouts",
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest),
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+};
 
-module.exports = withOffline(withMdxEnhanced(nextConfig)())
+module.exports = withOffline(withMdxEnhanced(nextConfig)());
