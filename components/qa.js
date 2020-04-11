48e72130defcadecd6d28e7eb77c38538f099ede
Jacob Roush <jacobroushmail@gmail.com>
2020-04-10 22:23:04 -0500

    basic

--- components/qa.js
+++ components/qa.js
@@ -1,7 +1,24 @@
-const Qa = () => (
-    <div>
-        <h1>{'Q & A'}</h1>
-    </div>
-)
+import { useState, useEfffect } from 'react';
+
+function Qa() {
+    const [questions, setQuestions] = useState([{
+        "q": "example question?",
+        "a": "Example Answer."
+    }])
+    return (
+        <div>
+            <h1>{'Q & A'}</h1>
+            <ul>
+                {questions.map(question => (
+                    <li>{question.q}
+                        <ul>
+                            {question.a}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default Qa;
--- mdx-layouts/recipe.js
+++ mdx-layouts/recipe.js
@@ -1,13 +1,44 @@
 import Qa from '../components/qa.js'
+import styled from 'styled-components'
+
+
+
+const StyledImgContainer = styled.div`
+position: relative;
+    width: 100%;
+`;
+
+const StyledImage = styled.img.attrs({
+    src: '/1080-placeholder.jpg'
+})`
+    /* position: absolute;
+    left: 0;
+    top: 0; */
+    max-width: 100%;
+`;
+
+const StyledText = styled.h2`
+    z-index: 100;
+    position: absolute;
+    color: white;
+    font-size: 24px;
+    font-weight: bold;
+    left: 150px;
+    top: 350px;
+`;
 
 export default frontMatter => {
     return ({ children: content }) => {
         return (
-            <div>
+            <>
+                <StyledImgContainer>
+                    <StyledImage />
+                    <StyledText>{'test'}</StyledText>
+                </StyledImgContainer>
                 <h1>{frontMatter.title}</h1>
                 {content}
                 <Qa />
-            </div>
+            </>
         )
     }
 }
