ad019c73e83f4e4fa543a785fecd3398e37f22af
Jacob Roush <jacobroushmail@gmail.com>
2020-04-14 23:34:41 -0500

    added framer motion and outline beer creation

--- pages/how-is-beer-made.js
+++ pages/how-is-beer-made.js
@@ -2,8 +2,15 @@ import { Heading, Text } from "@chakra-ui/core"
 
 const BeerStoryPage = () => (
     <>
-        <Heading>{"How is beer made?"}</Heading>
-        <Text>{"A beverage more ancient than time"}</Text>
+        <Heading as={"h1"}>{"How is beer made?"}</Heading>
+        <Text>{"A beverage with over a thousand years of history."}</Text>
+        <Text>{"Let's take high-level look at some of what goes into making a modern beer."}</Text>
+        <Heading as={"h2"}>{'Ingredients'}</Heading>
+        <Heading as={"h2"}>{'Mashing'}</Heading>
+        <Heading as={"h2"}>{'Boil'}</Heading>
+        <Heading as={"h2"}>{'Fermentation'}</Heading>
+        <Heading as={"h2"}>{'Carbination'}</Heading>
+        <Text>{"Before xxxx this step would be skipped but alas.."}</Text>
     </>
 )
 
